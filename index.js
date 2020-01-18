d563c4195b0be3ada93ebe4ddfd01bb723496734
kalwalt <github@kalwaltart.it>
2020-01-18 19:22:35 +0100

    WIP - starting to develop a simple app

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const jsartoolkit5 = require("./jsartoolkit5/js/artoolkit.api.js");
 
 console.log('we are here !');
 console.log(jsartoolkit5);
+var camera_para = new jsartoolkit5.ARCameraParam('./jsartoolkit5/examples/Data/camera_para.dat');
+var ar = new jsartoolkit5.ARController(320, 240, camera_para);
