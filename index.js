eb74db0c6bc5eebfa89ec33012bc31df00c05631
kalwalt <github@kalwaltart.it>
2020-01-25 12:29:15 +0100

    need to setup type module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const jsartoolkit5 = require("./jsartoolkit5/js/artoolkit.api.js");
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+import jsartoolkit5 from "./jsartoolkit5/build/artoolkit.min.js";
+
 
 console.log('we are here !');
-console.log(jsartoolkit5);
-var camera_para = new jsartoolkit5.ARCameraParam('./jsartoolkit5/examples/Data/camera_para.dat');
+console.log(jsartoolkit5());
+const camera_para = jsartoolkit5.ARCameraParam('./jsartoolkit5/examples/Data/camera_para.dat');
 var ar = new jsartoolkit5.ARController(320, 240, camera_para);
